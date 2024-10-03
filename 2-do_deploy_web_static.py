91d486a9aa33561ac04e11ef97b17e43ca1839bf
Relebohile <156066146+RELEBOHILE-PHEKO@users.noreply.github.com>
2024-10-03 22:01:47 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,47 +1,53 @@
 #!/usr/bin/python3
-"""
-Fabric script based on the file 1-pack_web_static.py that distributes an
-archive to the web servers
-"""
+"""Fabric script based on the file 1-pack_web_static.py that distributes an
+archive to the web servers."""
 
 from fabric.api import put, run, env
 from os.path import exists
 
-# Define the web servers' IP addresses
+# Define the web server hosts
 env.hosts = ['52.23.237.49', '54.205.189.207']
 
+
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers."""
-    
-    # Check if the archive exists
+    """Distributes an archive to the web servers.
+
+    Args:
+        archive_path (str): The path to the archive to be deployed.
+
+    Returns:
+        bool: True if all operations are successful, otherwise False.
+    """
     if not exists(archive_path):
         return False
 
     try:
-        # Extract the filename and its name without the extension
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
+        # Extract the file name and name without extension
+        file_name = archive_path.split("/")[-1]
+        no_ext = file_name.split(".")[0]
+
+        # Define the release path
         path = "/data/web_static/releases/"
 
-        # Upload the archive to the /tmp/ directory of the web server
+        # Upload the archive to /tmp/
         put(archive_path, '/tmp/')
 
         # Create the release directory
         run('mkdir -p {}{}/'.format(path, no_ext))
 
-        # Uncompress the archive to the release directory
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        # Uncompress the archive
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, no_ext))
 
-        # Remove the archive from the web server
-        run('rm /tmp/{}'.format(file_n))
+        # Clean up the uploaded archive
+        run('rm /tmp/{}'.format(file_name))
 
-        # Move the contents from web_static to the release directory
+        # Move the contents of web_static
         run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
 
-        # Remove the now-empty web_static directory
+        # Remove the empty web_static directory
         run('rm -rf {}{}/web_static'.format(path, no_ext))
 
-        # Delete the existing symbolic link
+        # Remove the current symbolic link
         run('rm -rf /data/web_static/current')
 
         # Create a new symbolic link
@@ -50,5 +56,5 @@ def do_deploy(archive_path):
         return True
 
     except Exception as e:
-        print(f"Error: {e}")  # Print the error message for debugging
+        print(f"An error occurred: {e}")
         return False
