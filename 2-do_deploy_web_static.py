bcbf3311ee92e3ee824664643f603cd730f32cb6
Relebohile <156066146+RELEBOHILE-PHEKO@users.noreply.github.com>
2024-10-05 22:06:18 +0200

    Update 2-do_deploy_web_static.py

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
 """Fabric function sends archived static site to webservers"""
-import os
-from fabric.api import env, local, run, cd, lcd
 
-env.hosts = ['52.23.237.49', '54.205.189.207']
 
-def do_clean(number=0):
-    """
-    Delete out-of-date archives.
+from fabric.api import put, run, env
+from os.path import exists
+# remotely exexutes commands in both servers if run in one of them.
+env.hosts = ["52.23.237.49", "54.205.189.207"]
 
-    Args:
-        number (int): The number of archives to keep.
-            If number is 0 or 1, keeps only the most recent archive.
-            If number is 2, keeps the most and second-most recent archives, etc.
-    """
-    number = 1 if int(number) == 0 else int(number)
 
-    archives = sorted(os.listdir("versions"))
-    # Remove old archives
-    [archives.pop() for _ in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
+def do_deploy(archive_path):
+    """Archives to web-servers"""
+    if exists(archive_path) is False:
+        return False
+    # If archive isn't even there archive nothing
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        # uploads archived folder to /tmp/
+        put(archive_path, '/tmp/')
 
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        # Remove old web_static releases
-        [archives.pop() for _ in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+# commands to uncompress folder, delete compressed, delete link
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except BaseException:
+        return False
